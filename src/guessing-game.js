b007612d1fd8b1d1a410f1ad7527948f95ee666d
user42022 <andrekizilov@yandex.ru>
2023-02-12 22:55:31 +0300

    feat:implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min+this.max)/2)
     }
 
     lower() {
-
+        this.max = Math.round((this.min+this.max)/2)
     }
 
     greater() {
-
+        this.min = Math.round((this.min+this.max)/2)
     }
 }
 
